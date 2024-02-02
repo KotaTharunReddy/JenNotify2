56147baa1f3a4345a573405e1b93ec4ea54318b2
Kota Tharun Reddy <tharunreddykota2001@gmail.com>
2024-02-02 11:24:07 +0530

    third line added to First.java

--- First.java
+++ First.java
@@ -2,5 +2,6 @@ public class First {
         public static void main(String[] args) {
         System.out.println("First line added");
         System.out.println("New line added");
+        System.out.println("Third line added");
     }
 }
